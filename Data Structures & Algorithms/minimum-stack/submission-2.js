13f5992354668d9b2c9779f53f4b3d290990b9f5
spreethi93 <s.preethi93@gmail.com>
2026-06-11 08:44:01 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,43 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.min = [];
+
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+        val = Math.min(val, 
+            this.min.length === 0 ?
+            val :
+            this.min[this.min.length - 1]);
+        this.min.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+        this.min.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length -1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return Math.min(...this.stack);
+        return this.min[this.min.length - 1];
+    }
+}
