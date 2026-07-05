89eeb61473217cd92655adfe19ef477cad6e0a2e
spreethi93 <s.preethi93@gmail.com>
2026-07-05 14:34:06 +0530

    Add: coin-change - submission-11

---
+++ Data Structures & Algorithms/coin-change/submission-11.js
@@ -0,0 +1,69 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+
+    coinChange(coins, amount) {
+        const dp = new Array(amount + 1).fill(amount + 1);
+        dp[0] = 0;
+
+        for(let i = 0; i <= amount; i++) {
+            for(let c of coins) {
+                if(i - c >= 0) {
+                    dp[i] = Math.min(dp[i], 1 + dp[i - c]);
+                }
+            }
+        }
+
+
+        return dp[amount] > amount ? -1 : dp[amount] ;
+    }
+
+
+    // coinChange(coins, amount) {
+    //     const dpArr = new Array(amount + 1).fill(amount + 1);
+    //     dpArr[0] = 0;
+
+    //     for(let i = 0; i <= amount; i++) {
+    //         for(let c of coins) {
+    //             if((i - c) >= 0) {
+    //                 dpArr[i] = Math.min(dpArr[i], 1 + dpArr[i - c]);
+    //             }
+    //         }
+    //     }
+
+    //     return dpArr[amount] > amount ? -1 : dpArr[amount];
+    // }
+
+    // coinChange(coins, amount) {
+    //     //dp array
+    //     //dp[amount] = 1 + dp[amount - c];
+    //     const dpArr = new Array(amount + 1).fill(amount + 1);
+    //     dpArr[0] = 0;
+
+    //     for(let i = 1; i <= amount; i++) {
+    //         for(let c of coins) {
+    //             if( (i - c) >= 0) {
+    //                 dpArr[i] = Math.min(dpArr[i], 1 + dpArr[i-c]);
+    //             }
+    //         }
+    //     }
+    //     return dpArr[amount] > amount ? -1 : dpArr[amount];
+    // }
+    // coinChange(coins, amount) {
+    //     const dpArr = new Array(amount + 1).fill(amount + 1);
+    //     dpArr[0] = 0;
+    //     for(let i = 1; i <= amount; i++) {
+    //         for(let a of coins) {
+    //             if((i - a) >= 0) {
+    //                 dpArr[i] = Math.min(dpArr[i], 1 + dpArr[i - a]);
+    //             }
+
+    //         }
+    //     }
+
+    //     return dpArr[amount] > amount ? -1 : dpArr[amount];
+    // }
+}
