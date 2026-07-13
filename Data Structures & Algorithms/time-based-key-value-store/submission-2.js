cecfc4411200784deb8081237d4327d6d9ae1c0a
spreethi93 <s.preethi93@gmail.com>
2026-07-13 09:25:27 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.js
@@ -0,0 +1,41 @@
+class TimeMap {
+    constructor() {
+        this.keyStore = new Map();
+    }
+
+    /**
+     * @param {string} key
+     * @param {string} value
+     * @param {number} timestamp
+     * @return {void}
+     */
+    set(key, value, timestamp) {
+        if(!(this.keyStore.has(key))) {
+            this.keyStore.set(key, []);
+        }
+        this.keyStore.get(key).push([timestamp, value]);
+    }
+
+    /**
+     * @param {string} key
+     * @param {number} timestamp
+     * @return {string}
+     */
+    get(key, timestamp) {
+        let res = '';
+        let values = this.keyStore.get(key) || [];
+        
+        let l = 0, r = values.length - 1;
+        while(l <= r) {
+            const mid = l + Math.ceil((r-l) / 2);
+            if(values[mid][0] <= timestamp) {
+                res = values[mid][1];
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return res;
+    }
+}
