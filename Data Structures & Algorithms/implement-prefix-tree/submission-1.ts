837d8bf201981af95f6f547b2799237e890513e7
spreethi93 <s.preethi93@gmail.com>
2026-08-15 10:27:05 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.ts
@@ -0,0 +1,61 @@
+class TrieNode {
+    children;
+    isEndOfWord;
+    constructor() {
+        this.children = new Map();
+        this.isEndOfWord = false;
+    }
+}
+
+class PrefixTree {
+    root;
+
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    /**
+     * @param {string} word
+     * @return {void}
+     */
+    insert(word: string): void {
+        let curr = this.root;
+        for(let c of word){
+            if(!curr.children.has(c)) {
+                curr.children.set(c, new TrieNode());
+            }
+            curr = curr.children.get(c);
+        }
+        curr.isEndOfWord = true;
+    }
+
+    /**
+     * @param {string} word
+     * @return {boolean}
+     */
+    search(word: string): boolean {
+        let curr = this.root;
+        for(let c of word) {
+            if(!curr.children.has(c)){
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return curr.isEndOfWord;
+    }
+
+    /**
+     * @param {string} prefix
+     * @return {boolean}
+     */
+    startsWith(prefix: string): boolean {
+        let curr = this.root;
+        for(let c of prefix) {
+            if(!curr.children.has(c)){
+                return false;
+            }
+            curr = curr.children.get(c);
+        }
+        return true;
+    }
+}
