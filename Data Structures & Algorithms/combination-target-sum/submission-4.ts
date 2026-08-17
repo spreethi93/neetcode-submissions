12c7bd2933305d37c3e1eb8d15ec4c4a859c817d
spreethi93 <s.preethi93@gmail.com>
2026-08-17 09:22:46 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums: number[], target: number): number[][] {
+        const res = [];
+
+        function dfs(i, curr, total) {
+            if(total === target) {
+                res.push([...curr]);
+                return;
+            }
+
+            if(i >= nums.length || total > target) {
+                return;
+            }
+
+            curr.push(nums[i]);
+            dfs(i, curr, total + nums[i]);
+            curr.pop();
+            dfs(i + 1, curr, total);
+        }
+
+        dfs(0, [], 0);
+
+        return res;
+    }
+}
