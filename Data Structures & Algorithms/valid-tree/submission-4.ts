43df18fc5bb9fda320d7133e6b3782ab649d812f
spreethi93 <s.preethi93@gmail.com>
2026-08-22 19:45:42 +0530

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {boolean}
+     */
+    validTree(n: number, edges: number[][]): boolean {
+        if(edges.length > n - 1) return false;
+        const visited = new Set();
+        const myHashSet = new Map();
+        for(let i = 0; i < n; i++) {
+            myHashSet.set(i, []);
+        }
+        for(let [node, child] of edges) {
+            myHashSet.get(node).push(child);
+            myHashSet.get(child).push(node);
+        }
+
+        function dfs(node, parent): boolean {
+            if(visited.has(node)) return false;
+            visited.add(node);
+            for(let child of myHashSet.get(node)){
+                if(child == parent) continue;
+                if(!dfs(child, node)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return dfs(0, -1) && visited.size === n;
+    }
+}
