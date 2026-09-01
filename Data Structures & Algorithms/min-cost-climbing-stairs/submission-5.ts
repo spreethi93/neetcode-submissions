181084b1f5d9872a04f42f948a9fd1ae763f6c3d
spreethi93 <s.preethi93@gmail.com>
2026-09-01 06:41:48 +0530

    Add: min-cost-climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-5.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} cost
+     * @return {number}
+     */
+    minCostClimbingStairs(cost: number[]): number {
+        const cache = new Int32Array(cost).fill(-1);
+
+        function dfs(i) {
+            if(i >= cost.length) return 0;
+            if(cache[i] != -1) return cache[i];
+
+            return cache[i] = cost[i] + Math.min(dfs(i+1), dfs(i+2));
+        }
+
+        return Math.min(dfs(0), dfs(1));
+    }
+}
